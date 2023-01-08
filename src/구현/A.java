32db3d818643c9d28b65330d6025467102acc01f
swager253 <swager253@naver.com>
2023-01-08 23:36:37 +0900

    Initial commit

---
+++ src/구현/A.java
@@ -0,0 +1,37 @@
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class A {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        int sum = 0;
+
+        for(int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = i + 1; j < 9; j++) {
+                if(sum - arr[i] - arr[j] == 100) {
+                    for(int k = 0; k < 9; k++) {
+
+                        if(i == k || j == k) continue;
+
+                        System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+        }
+        br.close();
+    }
+}
