058b2a3d4d9d99a9812be9301091fdcb0d559f6c
swager253 <swager253@naver.com>
2023-06-06 00:03:51 +0900

    ㅎ

--- src/구현_연습/S3085.java
+++ src/구현_연습/S3085.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class S3085 {
 
     static char[][] arr;
+    static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,6 +23,67 @@ public class S3085 {
             }
         }
 
+        // 행 체크
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N - 1; j++) {
+                if(arr[i][j] != arr[i][j + 1]) {
+                    swap(arr, i, j, i, j + 1);
+                    rowCheck(N);
+                    colCheck(N);
+                    swap(arr, i, j, i, j + 1);
+                }
+            }
+        }
 
+        // 열 체크
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N - 1; j++) {
+                if(arr[j][i] != arr[j + 1][i]) {
+                    swap(arr, j, i, j + 1, i);
+                    rowCheck(N);
+                    colCheck(N);
+                    swap(arr, j, i, j + 1, i);
+                }
+            }
+        }
+
+        bw.write(max + 1 + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+    static void rowCheck(int N) {
+
+        // 행 체크
+        for(int i = 0; i < N; i++) {
+            int cnt = 0;
+            for(int j = 0; j < N - 1; j++) {
+                if(arr[i][j] == arr[i][j + 1]) {
+                    cnt++;
+                } else {
+                    cnt = 0;
+                }
+                max = Math.max(max, cnt);
+            }
+        }
+    }
+    static void colCheck(int N) {
+
+        for(int i = 0; i < N; i++) {
+            int cnt = 0;
+            for(int j = 0; j < N - 1; j++) {
+                if(arr[j][i] == arr[j + 1][i]) {
+                    cnt++;
+                } else {
+                    cnt = 0;
+                }
+                max = Math.max(max, cnt);
+            }
+        }
+    }
+    static void swap(char[][] arr, int a, int b, int c, int d) {
+        char tmp = arr[a][b];
+        arr[a][b] = arr[c][d];
+        arr[c][d] = tmp;
     }
 }
